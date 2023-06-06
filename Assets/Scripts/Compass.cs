a468822fdf4c3134f38ef244f116df192956f026
Kevin Nguyen <kevnguyen58@gmail.com>
2023-06-06 03:09:51 -0700

    compass

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> checkPoints;
+    private GameObject currCheckpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currCheckpoint = checkPoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = currCheckpoint.transform.position;
+        Vector3 relative = transform.parent.InverseTransformPoint(direction);
+        
+        float point = Mathf.Atan2(relative.y, -relative.x) * Mathf.Rad2Deg;
+
+        transform.localRotation = Quaternion.Euler(0, 0, -point);
+        //Debug.Log(rotation.ToString());
+        Debug.Log(transform.eulerAngles.ToString());
+    }
+}
